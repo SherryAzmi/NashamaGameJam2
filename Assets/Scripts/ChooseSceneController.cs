39e885afc7ed1d317c1d052d3516c6a5735303f8
SherryAzmi <202310978@students.asu.edu.jo>
2026-06-26 16:01:09 +0300

    finished MainScene

---
+++ Assets/Scripts/ChooseSceneController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+
+public class ChooseSceneController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text userNameText;
+
+    private void Start()
+    {
+        if (userNameText != null)
+        {
+            string playerName = PlayerPrefs.GetString("PlayerName", string.Empty);
+            userNameText.text = $"Welcome , {playerName} !";
+        }
+    }
+}
